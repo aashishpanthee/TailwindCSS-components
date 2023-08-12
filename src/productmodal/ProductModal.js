dc339e6ffa940c7ac76984e0dabf30a1e773d992
Aashish Panthee <aashishpanthee060@gmail.com>
2023-08-13 00:06:01 +0545

    Stupid changes to ProductModal.js

--- src/productmodal/ProductModal.js
+++ src/productmodal/ProductModal.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const ProductModal = () => {
-  return <div>ProductModal</div>;
+  return (
+    <div>
+      <h4>Nothing to see </h4>
+      <h5>I have done mistake here</h5>
+    </div>
+  );
 };
 
 export default ProductModal;
