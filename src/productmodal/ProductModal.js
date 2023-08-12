595e7fbd4afb6ccc4213c699aaca6228b839c97c
Aashish Panthee <aashishpanthee060@gmail.com>
2023-08-13 00:08:47 +0545

    changes in cherry branch

--- src/productmodal/ProductModal.js
+++ src/productmodal/ProductModal.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const ProductModal = () => {
-  return <div>ProductModal</div>;
+  return (
+    <div>
+      <h1>I wanted this and it is change from cherry branch.</h1>
+      <h5>Change FROM CHERRY BRANCH</h5>
+    </div>
+  );
 };
 
 export default ProductModal;
